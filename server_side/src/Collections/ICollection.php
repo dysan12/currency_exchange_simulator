892e9d4ee72498abd06398d555567096dfa92b59
Michal Gaj <michalgaj@onet.pl>
2017-05-26 20:36:42 +0200

    Added doc to Interface

--- server_side/src/Collections/ICollection.php
+++ server_side/src/Collections/ICollection.php
@@ -10,18 +10,18 @@ interface ICollection
      * @param $collection
      * @return mixed
      */
-    public function setCollection($collection);
+    public function setCollection($collection): void;
 
     /**
-     * Adding item to collection(if not loaded it creates its ones
-     * @param $value
-     * @param $index
+     * Adding item to collection(if collection is not loaded method creates its ones)
+     * @param $value - value of item
+     * @param $index - name of item
      * @return mixed
      */
-    public function addItem($value, $index);
+    public function addItem($value, $index): void;
 
     /**
-     * Get item from collection, if item do not exists throw ItemCollectionException
+     * Get item from collection, if item does not exist throw ItemCollectionException
      * @param $index
      * @return mixed
      */
