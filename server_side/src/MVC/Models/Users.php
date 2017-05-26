ef31bd00a453b76854c1c60881b012b7de1da0d2
Michal Gaj <michalgaj@onet.pl>
2017-05-26 22:47:30 +0200

    Added throwing exception in createUser

--- server_side/src/MVC/Models/Users.php
+++ server_side/src/MVC/Models/Users.php
@@ -7,7 +7,12 @@ use Currency\Database\IDbProvider;
 use Currency\Exceptions\DbQueryCreatingFailure;
 use Currency\Exceptions\DbQueryNoResultsException;
 use Currency\Exceptions\DbQueryValueNotUnique;
+use Currency\Exceptions\DbQueryValueTooLong;
 
+/**
+ * Class Users handles getting and modifying user data
+ * @package Currency\MVC\Models
+ */
 class Users extends Model
 {
     private $dbConnection;
@@ -17,6 +22,12 @@ class Users extends Model
         $this->dbConnection = $dbConnection;
     }
 
+    /**
+     * Get user details. Query based on passed userLogin.
+     * @param string $userLogin - login of user.
+     * @return array - user's details.
+     * @throws DbQueryNoResultsException - throws if user doesn't exist in database table.
+     */
     public function getUserDetails(string $userLogin): array
     {
         $sqlQuery = 'SELECT login, name, surname, email FROM users WHERE login = :userLogin';
@@ -30,11 +41,24 @@ class Users extends Model
         return $result;
     }
 
+    /**
+     * Creating user according to passed data. Default values are ['name' => 'user_name'].
+     * @param string $login - must be unique
+     * @param string $password
+     * @param string $email - must be unique
+     * @param array $defaultValues - for eg. user name
+     * @return int - result of creating
+     * @throws DbQueryCreatingFailure - throws if there would be problems with creating user
+     * @throws DbQueryValueNotUnique - throws if login or email wouldn't be unique
+     * @throws DbQueryValueTooLong - throws if any of passed values exceeded appropriate length
+     */
     public function createUser(string $login, string $password, string $email, array $defaultValues = [])
     {
         $sqlQuery = "INSERT INTO users (login, password, email, name) VALUES (:login, :password, :email, :name)";
-
         $valuesToBind = ['login' => $login, 'password' => $password, 'email' => $email, 'name' => $defaultValues['name'] ?? ''];
+
+        if (strlen($login) > 40 || strlen($password) > 60 || strlen($email) > 120 || strlen($defaultValues['name'] ?? '') > 40)
+            throw new DbQueryValueTooLong('length of values cannot exceed(letters): login(40), password(60), email(120), name(40)');
 
         if (!$this->checkValueAvailability('email', 'users',  $email))
             throw new DbQueryValueNotUnique('provided email is already in use');
