0a7161d2250de9dfc06df466ea167d1c3f4c2558
Michal Gaj <michalgaj@onet.pl>
2017-05-28 17:19:42 +0200

    Added API key

--- server_side/config.php
+++ server_side/config.php
@@ -7,3 +7,5 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'currency_exchange_simulator');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
+
+define('RATES_API_KEY', '12afd06febaa1fb0d7a55ca88eaf73d6');
