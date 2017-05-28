3150218f5ceb63d920b1987fa5ee103f53d856d6
Michal Gaj <michalgaj@onet.pl>
2017-05-28 17:21:00 +0200

    Removed slash

--- server_side/index.php
+++ server_side/index.php
@@ -4,12 +4,11 @@ require 'vendor/autoload.php';
 require 'config.php';
 require 'src/Routing/Routes/initRoutes.php';
 
-$globalVarsCollection = new \Currency\Collections\GlobalCollection();
+$globalVarsCollection = new Currency\Collections\GlobalCollection();
 $logsCreator = new Currency\Logs\LogsCreator();
 
 $router = new Currency\Routing\Router($routesCollection, $globalVarsCollection, $logsCreator);
-preg_match('/(?:currency)(.{0,70})/', $_SERVER['REQUEST_URI'], $matches);
-$url = explode('/', $_SERVER['REQUEST_URI']);
+preg_match('/(?:server_side)(.{0,70})/', $_SERVER['REQUEST_URI'], $matches);
 $router->run($matches[1], $_SERVER['REQUEST_METHOD']);
 $route = $router->getMatchedRoute();
 $controllerPath = 'Currency\MVC\Controllers\\' . $route->getControllerName();
